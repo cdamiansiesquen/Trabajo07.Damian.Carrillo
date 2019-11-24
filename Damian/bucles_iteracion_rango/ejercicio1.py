427c33a6512b6bd8c5c15743bc6fc14d39f79f8e
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:16:48 -0500

    Se a terminado el ejercicio 1

---
+++ Damian/bucles_iteracion_rango/ejercicio1.py
@@ -0,0 +1,10 @@
+# Imprimir los "A" primeros numeros divididos entre 4
+import os
+# input
+A = int (os.sys.argv [ 1 ])
+# iterador
+for i in range(A):
+    print (i / 4)
+# fin_iterador_en_rango
+
+print ( " Fin del bucle " )

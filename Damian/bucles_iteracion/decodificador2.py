5edb16619df32d550114b97b0b9d755d12fd1d6e
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:15:00 -0500

    Se a terminado el decodificador

---
+++ Damian/bucles_iteracion/decodificador2.py
@@ -0,0 +1,21 @@
+# Decodificar msg encriptado
+# A = Brasil
+# B = Argentina
+# C = Bolivia
+# D = Peru
+import os
+# input
+msg = os.sys.argv [ 1 ].upper ()
+# bucle
+for letra in msg:
+    if letra == "A":
+        print("Brasil")
+    if letra ==  "B":
+        print("Argentina")
+    if letra ==  "C":
+        print("Bolivia")
+    if letra ==  "D":
+        print("Peru")
+# fin_iterador
+
+print ( " Fin del bucle ")

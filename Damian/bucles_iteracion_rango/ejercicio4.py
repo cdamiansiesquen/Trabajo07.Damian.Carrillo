065f4b08b875f32b96633aed4e5ada407f4c4286
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:18:24 -0500

    Se a terminado el ejercicio 4

---
+++ Damian/bucles_iteracion_rango/ejercicio4.py
@@ -0,0 +1,10 @@
+# Imprimir los "x" primeros numeros multiplicado por 3 y sumado 2
+import os
+# input
+x = int (os.sys.argv [ 1 ])
+# iterador
+for i in range(x):
+    print ((i) * 3 + 2)
+# fin_iterador_en_rango
+
+print ( " Fin del bucle " )

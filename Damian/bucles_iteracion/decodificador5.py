dd6ab2f240622cce42d596dbdbdd35c9d300aa3e
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:16:27 -0500

    Se a terminado el decodificador 5

---
+++ Damian/bucles_iteracion/decodificador5.py
@@ -0,0 +1,42 @@
+# Decodificar msg encriptado
+# P = enero
+# Q = febrero
+# R = marzo
+# S = abril
+# T = mayo
+# U = junio
+# V = julio
+# W = agosto
+# X = septiembre
+# Y = octubre
+# Z = noviembre
+import os
+# input
+msg = os.sys.argv [ 1 ].upper ()
+# bucle
+for letra in msg:
+    if letra == "P":
+        print("enero")
+    if letra == "Q":
+        print("febrero")
+    if letra == "R":
+        print("marzo")
+    if letra == "S":
+        print("abril")
+    if letra == "T":
+        print("mayo")
+    if letra == "U":
+        print("junio")
+    if letra == "V":
+        print("julio")
+    if letra == "W":
+        print("agosto")
+    if letra == "X":
+        print("septiembre")
+    if letra == "Y":
+        print("octubre")
+    if letra == "Z":
+        print("noviembre")
+# fin_iterador
+
+print ( " Fin del bucle ")

dafcd7efe6fad560fbc1bbe32ce2a51ac5b99344
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:17:05 -0500

    Se a terminado el ejercicio 2

---
+++ Damian/bucles_iteracion_rango/ejercicio2.py
@@ -0,0 +1,10 @@
+# Imprimir los "x" primeros numeros multiplicados por 2
+import os
+# input
+x = int (os.sys.argv [ 1 ])
+# iterador
+for i in range(x):
+    print (i * 2)
+# fin_iterador_en_rango
+
+print ( " Fin del bucle " )

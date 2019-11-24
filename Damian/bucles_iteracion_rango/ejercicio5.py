b67d5d59ce684d8c1fcf2d33f258bd09c6ad2615
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:18:41 -0500

    Se a terminado el ejercicio 5

---
+++ Damian/bucles_iteracion_rango/ejercicio5.py
@@ -0,0 +1,11 @@
+# Imprimir los numeros que se encuentran entre "a" y "b" dividido entre 2
+import os
+# input
+a = int(os.sys.argv [ 1 ])
+b = int(os.sys.argv [ 2 ])
+# iterador
+for i in  range(a, b):
+    print ((i) / 2 )
+# fin_iterador_en_rango
+
+print ( " Fin del bucle " )

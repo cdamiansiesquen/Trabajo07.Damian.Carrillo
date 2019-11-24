c88f4b165147849ab3c63c00173c4acf245b5474
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-24 10:15:27 -0500

    Se a terminado el decodificador3

---
+++ Damian/bucles_iteracion/decodificador3.py
@@ -0,0 +1,24 @@
+# Decodificar msg encriptado
+# 1 = Hola
+# 2 = buenos dias
+# 3 = soy
+# 4 = alexis
+# 5 = vivo en illimo
+import os
+# input
+msg = os.sys.argv [ 1 ].upper ()
+# bucle
+for letra in msg:
+    if letra == "1":
+        print("Hola")
+    if letra ==  "2":
+        print("buenos dias")
+    if letra ==  "3":
+        print("soy")
+    if letra ==  "4":
+        print("alexis")
+    if letra ==  "5":
+        print("vivo en illimo")
+# fin_iterador
+
+print ( " Fin del bucle ")
